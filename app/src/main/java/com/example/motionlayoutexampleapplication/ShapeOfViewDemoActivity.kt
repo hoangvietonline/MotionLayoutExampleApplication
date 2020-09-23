0c0e7b8fb6b394db61f4e916fd5ff3d263a59268
hoangvietonline <hoangvietqlr@gmail.com>
2020-09-23 14:17:39 +0700

    add motionLayout + shapeofview

---
+++ app/src/main/java/com/example/motionlayoutexampleapplication/ShapeOfViewDemoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.motionlayoutexampleapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShapeOfViewDemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shape_of_view_demo)
+    }
+}
